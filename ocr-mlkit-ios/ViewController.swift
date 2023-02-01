805b3b7ab2f7720121b24a49223f438d8b20cf9f
hyebin <developerhyebin@gmail.com>
2023-02-01 23:41:16 +0900

    Initial Commit

---
+++ ocr-mlkit-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ocr-mlkit-ios
+//
+//  Created by hyebin on 2023/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
